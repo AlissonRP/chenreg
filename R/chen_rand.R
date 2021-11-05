d49f3112f2bc3de6aee43f9492a013e3e5627071
AlissonRP <alirpereira@hotmail.com>
2021-11-05 16:10:57 -0300

    rand_numbers

---
+++ R/chen_rand.R
@@ -0,0 +1,18 @@
+#' chen_rand
+#'
+#'  random generation for the chen distribution.
+#'
+#' @param n  number of observations. If length(n) > 1, the length is taken to be the number required
+#'
+#' @param  tau   a number that indicates the quantile
+#'
+#' @param lambda shape parameter
+#'
+#' @details The  reparameterized chen distribution has density:
+#' \deqn{f(y;\lambda,\mu,\tau)=\dfrac{log(1-\tau)}{1-exp(\mu^\lambda)}\lambda y^{\lambda-1}exp\bigg[\dfrac{log(1-\tau)}{1-exp(\mu^\lambda]}[1-exp(y^\lambda)+y^\lambda \bigg]}
+#'
+chen_rand <- function(n, md, lambda, tau) {
+  u <- runif(n)
+  y <- (log(1 - (log(1 - u) / (log(1 - tau) / (1 - exp(md^lambda))))))^(1 / lambda)
+  return(y)
+}
