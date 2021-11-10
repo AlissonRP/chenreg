597c47e8dd7144ba9dd57f22bda77d4e7ee2fa6f
AlissonRP <alirpereira@hotmail.com>
2021-11-10 00:36:01 -0300

    non zero p-values

--- R/chen_summary.R
+++ R/chen_summary.R
@@ -3,10 +3,8 @@
 chen_summary <- function(z){
   model_presentation <- cbind(round(z$coef, 4), round(z$stderror, 4), round(z$zstat, 4), round(z$pvalues, 4))
   colnames(model_presentation) <- c("Estimate", "Std. Error", "z value", "Pr(>|z|)")
-<<<<<<< HEAD
   model_presentation[,4]<-  format.pval(model_presentation[,4], eps = .001, digits = 2)
-=======
->>>>>>> 498dd448a5f475f96be3e7bd0e53fc07db7791a9
+
   as.data.frame(model_presentation)
 
 }
