99682aadc27028418aa6e8bd7cd9577eac51424d
AlissonRP <alirpereira887@gmail.com>
2023-06-05 14:23:32 -0300

    first version of auto chen

--- R/auto_chen.R
+++ R/auto_chen.R
@@ -5,7 +5,7 @@
 #' on the metrics that you interested
 
 #'
-#' @param y 	is the name of the target variable:
+#' @param y 	is the name of the target variable
 #'
 #' @param  data   data frame, list or environment (or object coercible by
 #' as.data.frame to a data frame)
@@ -19,19 +19,20 @@
 #'
 #' @param total number of combinations
 #'
-#' @param info if is TRUE will print in console the best combination of variables
+#' @param info if is TRUE will print in console more information
 #'
 #' @examples
 #' library(chenreg)
 #' data <- simu[, -1]
-#' auto_chen("Y", data, metric = "bic", info = T)
+#'
+#' auto_chen("Y", data, metric = "rmse", total = 3)
+#' auto_chen("Y", data, metric = "bic", info = TRUE)
 #'
 #'
 #' @note
-#' You can specify all variables (except y) to be your covariates using `.`, you
-#' can also add transformation like `log(x1)`
-#' You can write -1 in the formula to not use intercept in the model.
-#' @return chenReg returns an object of class `chenreg`
+#' the default value of `total` is 50, but if 50
+#' is more than the possible number of combinations `(k)`,
+#' then `k` will be used as the total.
 
 
 
@@ -39,7 +40,13 @@
 
 
 
+#' @export
 auto_chen <- function(y, data, metric = "rmse", quantile = 0.5, alpha = 0.05, total = NULL, info = FALSE) {
+
+  if ((((0 < quantile) && (quantile < 1)) && ((0 < alpha) && (alpha < 1))) == FALSE){
+    stop("The quantile and alpha must be between 0 and 1")
+  }
+
   x_names <- data[, !(names(data) %in% y)] |> names()
 
 
@@ -49,12 +56,23 @@ auto_chen <- function(y, data, metric = "rmse", quantile = 0.5, alpha = 0.05, to
 
   max_combinations <- do.call("c", lapply(seq_along(x_names), \(i) combn(x_names, i, FUN = list)))
 
+
   if (is.null(total) == TRUE) {
     total <- 50
     if (total > length(max_combinations)) {
       total <- length(max_combinations)
     }
   }
+
+
+  if (total > length(max_combinations)) {
+    warning("The total number of models you want to adjust is greater than the possible quantity.
+The total value has been adjusted to the maximum possible.")
+    total <- length(max_combinations)
+  }
+
+
+
 
   combinations <- sample(max_combinations, total)
   results <- data.frame(
@@ -65,17 +83,22 @@ auto_chen <- function(y, data, metric = "rmse", quantile = 0.5, alpha = 0.05, to
     "significants" = 1,
     "normal_residuals" = "ok"
   )
+  n = 0
+  count_errors = list()
 
   for (i in 1:length(combinations)) {
     X <- data[combinations[i] |> unlist()]
     Y <- data[y]
-
-    models <- chen_reg(data.frame(Y, X), Y ~ ., quantile = quantile)
+    tryCatch(
+      expr = {
 
     vars <- paste(X |> names(), collapse = " + ")
+    models <- chen_reg(data.frame(Y, X), Y ~ ., quantile = quantile)
+
     significants <- (\(x) x < alpha)(models$pvalues) |> sum()
     normal_test <- (models$residuals |> shapiro.test())$p.value
-    check_residuals <- (ifelse(normal_test > 0.05, "OK", "not-normal"))
+    check_residuals <- (ifelse(normal_test > alpha, "OK", "not-normal"))
+
 
     results <- results |> rbind(data.frame(
       "variables" = vars,
@@ -86,6 +109,27 @@ auto_chen <- function(y, data, metric = "rmse", quantile = 0.5, alpha = 0.05, to
       "significants" = significants - 1,
       "normal_residuals" = check_residuals
     ))
+      },
+
+    error = function(e){
+      count_errors <<- c(count_errors, list(e))
+
+      if (info == TRUE){
+      message(paste("the model with ", vars ," not fitted because of an error"))
+
+
+      print(count_errors)
+      }
+
+    }
+
+
+)
+  n = n + 1
+  if (n == length(combinations)){
+    message(paste('All done. ', 'Out of a total of', total,",", length(count_errors), 'models were not adjusted due to error'))
+  }
+
   }
   results <- results[-1, ]
   results <- results[order(results[metric] |> unlist()), ]
@@ -93,7 +137,7 @@ auto_chen <- function(y, data, metric = "rmse", quantile = 0.5, alpha = 0.05, to
 
 
   if (info == TRUE) {
-    cat(paste("The best variables based on", metric, "is", results[1, 1], "\n"))
+    message(paste("The best variables based on", metric, "is", results[1, 1], "\n"))
   }
 
 
--- tests/testthat/test-auto_chen.R
+++ tests/testthat/test-auto_chen.R
@@ -1,9 +1,16 @@
 library(chenreg)
-data <- simu[, -1]
-best = auto_chen("Y", data, metric = "bic", info = T)
-
-
+data = simu
 
 test_that("auto_chen works", {
+  best = auto_chen("Y", data, metric = "bic")
+
+
+
+
   expect_equal(best[1,1], "V2 + V3 + V4")
+  expect_error(auto_chen("Y", data, metric = "bic", alpha = 2), "The quantile and alpha must be between 0 and 1")
+  expect_error(auto_chen("Y", data, metric = "bic", quantile = 2), "The quantile and alpha must be between 0 and 1")
+  expect_warning(auto_chen("Y", data, metric = "bic", total = 70), "The total number of models you want to adjust is greater than the possible quantity.
+The total value has been adjusted to the maximum possible.")
+
 })
