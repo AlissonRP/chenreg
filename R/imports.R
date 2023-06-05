b90fc668c59cf573fabeeea4e2ae97e8fa07bea0
AlissonRP <alirpereira887@gmail.com>
2023-06-05 15:24:51 -0300

    add new version of R

--- R/imports.R
+++ R/imports.R
@@ -6,6 +6,7 @@
 #' @importFrom stats shapiro.test
 #' @importFrom graphics hist
 #' @importFrom utils combn
+#' @importFrom graphics par
 utils::globalVariables(c(
   "V1", "tau", "y", "X", "formula", "coef", "runif",
   "ginv_lig", "quantile", "residc"
