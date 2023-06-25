317efc01a8f4afbee45c6ff0f81e0f8797cd9d55
AlissonRP <alirpereira887@gmail.com>
2023-06-25 11:11:34 -0300

    plot and predict OK

--- R/plot.chenreg.R
+++ R/plot.chenreg.R
@@ -9,14 +9,28 @@
 
 plot.chenreg <- function(x, ...) {
   data = data.frame(residuals = x$residuals, index = 1:length(x$residuals))
+  add_margin = 0.05
+  y_lim_sup = max(c(3 + add_margin, max(x$residuals) + add_margin))
+  y_lim_inf = min(c(-3 - add_margin, min(x$residuals) - add_margin))
+
+
     par(ask=TRUE)
     hist(data$residuals,
          main = "Quantile Residuals",
          xlab = "Values"
     )
+
+
+
     plot(data$residuals,
          main = "Residuals vs Index",
-         ylab = "Residuals")
+         ylab = "Residuals", ylim=c(y_lim_sup, y_lim_inf))
+    abline(2, 0, lty=6)
+    abline(-2, 0, lty=6)
+    abline(3, 0, lty=2)
+    abline(-3, 0, lty=2)
+
+
 
     plot(x$residuals, x$fitted.values,
          xlab = "Residuals",
@@ -24,8 +38,15 @@ plot.chenreg <- function(x, ...) {
          main = "Residuals vs Fitted Values")
 
     car::qqPlot(x$residuals, col=c("black"), col.lines = "black",
-                ylab = "Residuals", xlab = "Norm Quantiles")
+                ylab = "Residuals", xlab = "Norm Quantiles",
+                main = "Confidence Envelope")
 
     par(ask=FALSE)
 
   }
+
+
+
+
+
+
